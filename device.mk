6f3f70dff53133837559ec10bfdc7e259417f65c
Deivid21 <david.parra.ignacio@gmail.com>
2021-06-16 15:41:36 -0400

    Revert "nash: Drop Lineage Touch"
    This reverts commit 88d2a664241f25086c4a7fb50c5a74420f7a2c17.

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.nash
 
+# Lineage hardware
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.nash
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
